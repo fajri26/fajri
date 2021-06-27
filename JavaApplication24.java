898eead9d2afb88c6db2621ddc3ef597563b5d11
fajri26 <85241860+fajri26@users.noreply.github.com>
2021-06-28 01:27:28 +0700

    Add files via upload

---
+++ JavaApplication24.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication24;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JavaApplication24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int a,b,c;
+        double kali,bagi,tambah,kurang;
+        // memberikan nilai pada variabel      
+        a=10;
+        b=6;  
+        c=23;
+        //menentukan rumus
+        kali=a*b;
+        bagi=c/b;
+        tambah=c+c;
+        kurang=c-a;
+        //Cetak nilai 
+        System.out.println("kali: "+kali);
+        System.out.println("bagi: "+bagi);
+        System.out.println("tambah: "+tambah);
+        System.out.println("kurang: "+kurang);
+    }   
+}
+    }
+    
+}
